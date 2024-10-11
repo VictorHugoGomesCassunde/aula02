8f66a0e5eb3eeb2c9d5ed5cce8a1db03981b59e1
VictorHugoGomesCassunde <victorhugocassunde10@gmail.com>
2024-10-10 20:23:55 -0400

    10/10/24
    terminar

--- src/exemplo3/index.jsx
+++ src/exemplo3/index.jsx
@@ -2,25 +2,28 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   
-  const [??,??] = useState([]);
+  const [buscar,setBuscar] = useState([]);
 
   useEffect(() => { 
-
-    const  ????  = async () => {
-        const resposta = await fetch('https://randomuser.me/api');
+    const buscarUsuario = async () => {
+        const resposta = await fetch('https://fakestoreapi.com/products');
         const dados = await resposta.json();
-         //complete o código
+        setBuscar(dados);
     }
-    //complete o código
-  }, []);
+  buscarUsuario();
+  }, []);//complete o código);
 
   return (
     <>
-      <h1>Usuário</h1>
+      <h1>Usuario</h1>
       <ul>
-        {
-            //complete o código
-        }
+        {buscar.map(usuario => (
+          <li key={usuario.userId}>
+          <h2>{usuario.id}</h2>
+          <p>{usuario.title}</p>
+          <p>{usuario.completed}</p>
+</li>
+        ))}
       </ul>
     </>
   );
